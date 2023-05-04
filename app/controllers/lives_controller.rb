22b157deeba939657430980338261a94195cb7ea
Giulio Mauriello <giuliom.90@live.it>
2023-05-04 20:07:34 +0200

    new and create method in lives controller created

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,2 +1,19 @@
 class LivesController < ApplicationController
+
+  def new
+    @life = Life.new
+  end
+
+  def create
+    @life = Life.new(life_params)
+    @life.save
+    redirect_to new_life_path(@life)
+  end
+
+  private
+
+  def life_params
+    params.require(:life).permit(:name, :details, :user_id)
+  end
+
 end
