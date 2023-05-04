a97daf7f313d96cf6e2fa6649186e37fc07e7969
katya sitko <kat.sitko@gmail.com>
2023-05-04 20:05:18 +0200

    created destroy method and added routes- resources for Bookings

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,2 +1,7 @@
 class LivesController < ApplicationController
+  def delete
+    @life = Lives.find(params[:id])
+    @life.destroy
+    redirect_to lives_path, status: :see_other
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :lives
+  resources :lives do
+    resources :bookings, only: %i[index show destroy create new]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
