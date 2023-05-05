16277fdedbb1e7786da874c3c9ab921f9a9fe7db
katya sitko <kat.sitko@gmail.com>
2023-05-05 12:19:26 +0200

    adding booking controller methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,13 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new()
+    @booking.save
+    redirect_to new_life_booking(@booking)
+  end
+
 end
--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,15 +1,15 @@
 class LivesController < ApplicationController
-  def destroy
-    @life = Lives.find(params[:id])
-    @life.destroy
-    redirect_to lives_path, status: :see_other
-  end
-
   def index
     @lives = Life.all
   end
 
   def show
     @life = Life.find(params[:id])
+  end
+
+  def destroy
+    @life = Lives.find(params[:id])
+    @life.destroy
+    redirect_to lives_path, status: :see_other
   end
 end
