294b8e33b2e9da8cdfe94db3ecb96897d6b016cc
katya sitko <kat.sitko@gmail.com>
2023-05-09 11:55:51 +0200

    added Address column to Lives

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_09_095024) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_09_095431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
