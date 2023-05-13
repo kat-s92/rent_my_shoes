53368fcef62e6f8102d20c1dfa643735976a1d77
katya sitko <kat.sitko@gmail.com>
2023-05-13 14:37:58 +0200

    creating lives

--- db/seeds.rb
+++ db/seeds.rb
@@ -121,107 +121,107 @@ puts "Creating Lives 🗂️"
 # end
 
 
-life_jesus = Life.new do |life|
-  life.address = ["Stationsplein, 1012 Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy",
-                  "Pariser Platz, 10117 Berlin, Germany", "Normannenstraße 20, 10365 Berlin",
-                  "Friedrichstraße 43, 10117 Berlin, Germany",
-                  "Rudi-Dutschke-Straße 26, 10969 Berlin",
-                  "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris",
-                  "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, Dublin 1, D01 F5P2, Ireland"].sample
-  life.name = "Jesus"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-# life.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-end
-life_jesus.user = user_jesus
-life_jesus.save
+# life_jesus = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy",
+#                   "Pariser Platz, 10117 Berlin, Germany", "Normannenstraße 20, 10365 Berlin",
+#                   "Friedrichstraße 43, 10117 Berlin, Germany",
+#                   "Rudi-Dutschke-Straße 26, 10969 Berlin",
+#                   "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris",
+#                   "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, Dublin 1, D01 F5P2, Ireland"].sample
+#   life.name = "Jesus"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# # life.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+# end
+# life_jesus.user = user_jesus
+# life_jesus.save
 
-life_harry_potter = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy",
-    "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Harry Potter"
-  life.details = "I am an orphan living with my abusive aunt and uncle. Last year, I discovered I am a wizard and now I study at Hogwarts School of Witchcraft and Wizardry. Come do magic like me!"
-end
-life_harry_potter.user = user_harry_potter
-life_harry_potter.save
+# life_harry_potter = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy",
+#     "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Harry Potter"
+#   life.details = "I am an orphan living with my abusive aunt and uncle. Last year, I discovered I am a wizard and now I study at Hogwarts School of Witchcraft and Wizardry. Come do magic like me!"
+# end
+# life_harry_potter.user = user_harry_potter
+# life_harry_potter.save
 
-life_elon_musk = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Elon Musk"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-end
-life_elon_musk.user = user_elon_musk
-life_elon_musk.save
+# life_elon_musk = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Elon Musk"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# end
+# life_elon_musk.user = user_elon_musk
+# life_elon_musk.save
 
-life_beyonce = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Beyoncé"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-end
-life_beyonce.user = user_beyonce
-life_beyonce.save
+# life_beyonce = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Beyoncé"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# end
+# life_beyonce.user = user_beyonce
+# life_beyonce.save
 
-life_stephen_hawking = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Stephen Hawking"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-end
-life_stephen_hawking.user = user_stephen_hawking
-life_stephen_hawking.save
+# life_stephen_hawking = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Stephen Hawking"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# end
+# life_stephen_hawking.user = user_stephen_hawking
+# life_stephen_hawking.save
 
-life_michelle_obama = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Michelle Obama"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-end
-life_michelle_obama.user = user_michelle_obama
-life_michelle_obama.save
+# life_michelle_obama = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Michelle Obama"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# end
+# life_michelle_obama.user = user_michelle_obama
+# life_michelle_obama.save
 
-life_serena_williams = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Serena Williams"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-end
-life_serena_williams.user = user_serena_williams
-life_serena_williams.save
+# life_serena_williams = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Serena Williams"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# end
+# life_serena_williams.user = user_serena_williams
+# life_serena_williams.save
 
-life_van_gogh = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Van Gogh"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-end
-life_van_gogh.user = user_van_gogh
-life_van_gogh.save
+# life_van_gogh = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Van Gogh"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# end
+# life_van_gogh.user = user_van_gogh
+# life_van_gogh.save
 
-life_emily_henry = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Emily Henry"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-end
-life_emily_henry.user = user_emily_henry
-life_emily_henry.save
+# life_emily_henry = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Emily Henry"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# end
+# life_emily_henry.user = user_emily_henry
+# life_emily_henry.save
 
-life_agatha_christie = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Agatha Christie"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-end
-life_agatha_christie.user = user_agatha_christie
-life_agatha_christie.save
+# life_agatha_christie = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Agatha Christie"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# end
+# life_agatha_christie.user = user_agatha_christie
+# life_agatha_christie.save
 
-life_jamie_oliver = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Jamie Oliver"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-end
-life_jamie_oliver.user = user_jamie_oliver
-life_jamie_oliver.save
+# life_jamie_oliver = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Jamie Oliver"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# end
+# life_jamie_oliver.user = user_jamie_oliver
+# life_jamie_oliver.save
 
-life_hasan_minhaj = Life.new do |life|
-  life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
-  life.name = "Hasan Minhaj"
-  life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
-end
-life_hasan_minhaj.user = user_hasan_minhaj
-life_hasan_minhaj.save
+# life_hasan_minhaj = Life.new do |life|
+#   life.address = ["Stationsplein, 1012 AB Amsterdam, Netherlands", "Piazza del Colosseo, 1, 00184 Roma RM, Italy", "Pariser Platz, 10117 Berlin", "Normannenstraße 20, 10365 Berlin", "Friedrichstraße 43, 10117 Berlin", "Rudi-Dutschke-Straße 26, 10969 Berlin", "Pl. Charles de Gaulle, 75008 Paris", "Champ de Mars, 5 Av. Anatole France, 75007 Paris", "London SW1A 0AA, United Kingdom", "O'Connell Street Lower, North City, Dublin 1, D01 F5P2, Ireland" ].sample
+#   life.name = "Hasan Minhaj"
+#   life.details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non efficitur dui."
+# end
+# life_hasan_minhaj.user = user_hasan_minhaj
+# life_hasan_minhaj.save
 
-puts "We have a DB ✅ with #{Life.count} lives"
+# puts "We have a DB ✅ with #{Life.count} lives"
