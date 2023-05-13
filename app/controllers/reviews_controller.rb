4cdd657d7fbaf7c30afc06084ac3ae727aff8057
Aishwarya Kanitkar <aishwaryakanitkar25@gmail.com>
2023-05-13 11:40:19 +0200

    Added the stars on the lives index page for average reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 require 'date'
 class ReviewsController < ApplicationController
-  # def index
-  #   @reviews = Review.all
-  # end
+  def index
+    @reviews = Review.all
+  end
 
-  # def show
-  #   @review = Review.find(params[:id])
-  # end
+  def show
+    @review = Review.find(params[:id])
+  end
 
   def new
     @review = Review.new
